99a6274eaafede9ba52dc4a6f8b0414130fcbeaa
MOHAMMAD ANIK <161469632+MOHAMMAD-ANIK@users.noreply.github.com>
2025-02-17 18:00:17 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch();
-  console.log('Puppeteer Version:', puppeteer.version); // ✅ ফাংশন নয়, তাই () লাগবে না
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox'] // ✅ স্যান্ডবক্স বন্ধ করা হলো
+  });
+  console.log('Puppeteer Version:', puppeteer.version);
   const page = await browser.newPage();
   await page.goto('https://example.com');
   console.log('Page Title:', await page.title());
